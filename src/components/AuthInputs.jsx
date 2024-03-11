eec5841062d21c4586700c97a996b54ac6488768
mila2999 <mila.savonik@gmail.com>
2024-03-11 13:20:25 +0200

    Migrated background of inputs to Tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 import Button from './Button';
 import Input from './Input';
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -31,8 +23,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           label="Email"
           invalid={emailNotValid}
@@ -45,7 +40,7 @@ export default function AuthInputs() {
           type="password"
           onChange={(event) => handleInputChange('password', event.target.value)}
         />
-      </ControlContainer>
+      </div>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
